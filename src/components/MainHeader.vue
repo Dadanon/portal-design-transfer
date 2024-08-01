e37b53b9992223212a976979050872f1a4ce8a83
Gubenko_Daniil <gubenko@ia-group.ru>
2024-08-01 18:57:22 +0300

    First steps

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const url_logo = '/urbanset_logo.png'
+</script>
+
+<template>
+  <el-row justify="center">
+    <el-image :src="url_logo" :fit="'fill'" />
+    <h2>ПЛАТФОРМА РАЗВИТИЯ ГОРОДОВ</h2>
+  </el-row>
+</template>
+
+<style scoped>
+
+</style>
